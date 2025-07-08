7702adfdfb78dbd635718a19cd4a12b39139fd53
Anusha M G <anushasajjan0111@gmail.com>
2025-07-08 14:07:58 +0000

    operations added

---
+++ js-basics/03_operations.js
@@ -0,0 +1,40 @@
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2%2)
+// console.log(2/2)
+
+let str1 =  "hello"
+let str2 = " Anusha"
+// console.log(str1+str2);
+
+
+
+// dont code like this 
+
+// console.log("1"+2)//  12
+// console.log(1+"2") //12
+// console.log(2+"1"+2) //212
+// console.log(2+2+"1") //41
+
+
+// console.log((3+4)*1%2)
+
+// console.log(+true)
+// console.log(+"")
+
+
+// in coding readability is imp, so dont show ur smatness and tricky code in the interview or in company(hihihi)
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+// unary operators in js
+// pre-increment, post-increment, pre-decrement and post-decrement
+// for more explanation explore mdn file
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
